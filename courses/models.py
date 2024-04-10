77b480060e0a80a9387738433285b1032377d577
ElnaraGasanova <elnaraskypro@gmail.com>
2024-04-11 00:25:27 +0300

    Описаны поля Курсов.

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from users.models import NULLABLE
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Курс', help_text='Укажите наименование курса')
+    image = models.ImageField(upload_to='courses_image', verbose_name='Превью', help_text='Загрузите превью курса',
+                              **NULLABLE)
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        '''Добавляем строковое отображение это будет выводиться на сайте в карточке!'''
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
