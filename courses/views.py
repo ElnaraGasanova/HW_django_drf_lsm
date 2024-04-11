37ebf25ee3f63dc3069312d890dc296a25f55f08
ElnaraGasanova <elnaraskypro@gmail.com>
2024-04-11 20:24:08 +0300

    Описан CRUD.

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from courses.models import Course
+from courses.serializers import CourseSerializer
 
-# Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    '''Описываем ViewSet для работы с моделью.'''
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from lessons.models import Lesson
+from lessons.serializers import LessonSerializer
 
-# Create your views here.
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    '''Описываем контроллеры на основе дженерик.'''
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    '''Описываем контроллеры на основе дженерик.'''
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
